578eda8c88ebcfc54676a73a8b3bfea269715e13
Mark Holtzhausen <nemesarial@gmail.com>
2021-04-26 23:22:37 +0200

    Bootstrap app

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,71 +24,21 @@ type FormValues = {
 }
 
 export default function Home() {
-  const [status, setStatus] = useState<'initial' | 'error' | 'success'>('initial');
-  const [message, setMessage] = useState('');
-  const [loading, setLoading] = useState<Boolean>(false);
-  const [form] = Form.useForm();
-
-  const onFinish = async ({ link }: FormValues) => {
-    setLoading(true)
-    try {
-      const response = await axios.post<ShortenLinkResponse>('/api/shorten_link', { link });
-      setStatus('success');
-      setMessage(response.data?.short_link);
-      setLoading(false)
-    }
-    catch(e) {
-      const error = e as AxiosError<ShortenLinkError>;
-      setStatus('error');
-      setMessage(error.response?.data?.error_description || 'Something went wrong!');
-      setLoading(false)
-    }
-  }
-
-  const onFinishedFailed = () => {
-    setStatus('error');
-    const error = form.getFieldError('link').join(' ');
-    setMessage(error);
-  }
+  // const [status, setStatus] = useState<'initial' | 'error' | 'success'>('initial');
+  // const [form] = Form.useForm();
 
   return (
     <Layout>
       <Head>
-        <title>Shorty</title>
+        <title>{process.env.NEXT_PUBLIC_APP_TITLE}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header>
-        <div className={styles.logo} >Shorty</div>
+        <div className={styles.logo} >{process.env.NEXT_PUBLIC_APP_TITLE}</div>
       </Header>
       <Content className={styles.content}>
         <div className={styles.shortner}>
-          <Title level={5}>Copy &amp; Paste your lengthy link</Title>
-          <Form
-            form={form}
-            onFinish={onFinish}
-            onFinishFailed={onFinishedFailed}
-          >
-            <div className={styles.linkField}>
-              <div className={styles.linkFieldInput}>
-                <Form.Item name="link" noStyle rules={[{
-                  required: true,
-                  message: 'Please paste a correct link',
-                  type: 'url',
-                }]}>
-                  <Input placeholder="https://my-super-long-link.com/bla bla" size="large"/>
-                </Form.Item>
-              </div>
-              <div className={styles.linkFieldButton}>
-                <Form.Item>
-                  <Button type="primary" htmlType="submit" style={{ width: '100px' }} size="large">
-                    {loading?<LoadingOutlined />:'Shorten!'}
-                    
-                  </Button>
-                </Form.Item>
-              </div>
-            </div>
-          </Form>
-          {['error', 'success'].includes(status) && (<Alert showIcon message={message} type={status as 'error' | 'success'} />)}
+          Hello world
         </div>
       </Content>
       <Footer className={styles.footer}>
